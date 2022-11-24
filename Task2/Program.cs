f95265c25733db4bf5277e4815af923d06544871
Natasha <n.chalganova@mail.ru>
2022-11-24 16:29:31 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32 (Console.ReadLine());
+if (A > B) 
+{
+    Console.WriteLine ("maximum =" + A);
+}
+else 
+{
+    Console.WriteLine ("maximum =" + B);
+}
