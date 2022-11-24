b96fa815e8a26c97e898e775f53beab96eb6521d
Natasha <n.chalganova@mail.ru>
2022-11-24 16:38:58 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+for (int a = 1; a <= n; a++)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine (a);
+    }
+}
